e10db84b57b1be7db66492b810dfbb883c5cef2f
yelo <zhihuzeye@gmail.com>
2013-11-27 17:22:55 +0800

    clean

--- test/app/case/base/preload.js
+++ test/app/case/base/preload.js
@@ -14,13 +14,6 @@ define(function (require, exports, module) {
 
   return function (preload) {
     describe('preload', function () {
-        // var test = function (ubb, text, output) {
-        //   describe(text, function () {
-        //     it('should be ' + output, function () {
-        //       expect(ubb.toHtml(text)).to.be.equal(output);
-        //     });
-        //   });
-        // };
       describe('img', function () {
         var cb = function (src, img) {
           console.log('called!');
@@ -35,13 +28,6 @@ define(function (require, exports, module) {
         it('should trigger the "after" method ', function () {
           expect(spy.calledOnce).to.be.true;
         });
-
-        // var ubb, text, output;
-        // ubb = new Ubb();
-        // ubb.add(testTag.pair);
-        // text = '[test foo=120%;"> data-height="]sth[/test]';
-        // output = '<div class="test" data-foo="120%;&quot;&gt;">sth</div>';
-        // test(ubb, text, output);
       });
     });
   };
